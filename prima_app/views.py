115e938a716d4024565b2d2930778d3e17797ccf
Rickmay2005 <riccardo.maggioni@alessandrinimainardi.edu.it>
2023-10-05 13:55:42 +0200

    update

--- prima_app/views.py
+++ prima_app/views.py
@@ -12,3 +12,14 @@ def lista(request):
 
 def chisiamo(request):
     return render(request, "chisiamo.html")
+
+def variabili(request):
+    context={
+        'var1':'Prima variabile',
+        'var2':'Seconda variabile',
+        'var3':'Terza variabile'
+    }
+    return render(request, "variabili.html", context)
+
+def index(request):
+    return render(request, "index.html")
